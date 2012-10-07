2cae2003559c58e6916c0eedfd55a4c4c6220798
manojnoob <manoj.p.gudi@gmail.com>
2012-10-07 22:36:56 +0530

    adding opencv stuff

---
+++ pps_mj/CMakeFiles/DisplayImage.dir/DependInfo.cmake
@@ -0,0 +1,13 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/xub/testing/pca_in_scilab/pps_mj/DisplayImage.cpp" "/home/xub/testing/pca_in_scilab/pps_mj/CMakeFiles/DisplayImage.dir/DisplayImage.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
